2ce3596c4f15d37d2980ecf775195c0f5d3b4dda
songnulee <thdsnfl6783@naver.com>
2016-07-20 19:16:11 +0900

    parsing-text2

--- app/src/main/java/uz/chamber/maroqand/pasing_text_test.java
+++ app/src/main/java/uz/chamber/maroqand/pasing_text_test.java
@@ -1,26 +1,54 @@
-/*package uz.chamber.maroqand;
+package uz.chamber.maroqand;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 import java.io.IOException;
 
-
-public class Pasing_text {
+public class pasing_text_test extends AppCompatActivity {
     String htmlPageUrl = "http://chamber.uz/en/page/3435";
 
+    TextView textviewHtmlDocument;
+
+    String htmlContentInStringFormat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
 
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+
+        textviewHtmlDocument = (TextView) findViewById(R.id.textView);
+
+
+        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod());
+
+        Button htmlTitleButton = (Button) findViewById(R.id.button);
+
+        assert htmlTitleButton != null;
+        htmlTitleButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+
+            public void onClick(View v) {
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute();
+            }
+        });
+    }
 
     private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
 
@@ -67,7 +95,3 @@ public class Pasing_text {
     }
 
 }
-
-
-
-*/
